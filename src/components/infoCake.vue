82e4eb335632ca715add9c0238f7af4880642576
Arturo Solís <69863981+ArturoOSc@users.noreply.github.com>
2022-03-30 23:31:41 -0600

    Add files via upload

---
+++ src/components/infoCake.vue
@@ -0,0 +1,104 @@
+<template>
+<h1 class="tituInfo">Realiza tu pedido</h1>
+    <section class="form-register"> 
+    <h4>Formulario de pedido</h4>
+    <input class="controls" type="text" name="nombres" id="nombres" placeholder="Nombre(s)">
+    <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Apellido(s)">
+    <input class="controls" type="email" name="correo" id="correo" placeholder="Correo: micorreo@example.com">
+    <input class="controls" type="telephone" name="telefono" id="telefono" placeholder="Teléfono">
+    <p>Estoy de acuerdo con <a href="#">Terminos y Condiciones</a></p>
+    <input class="botons" type="submit" value="Registrar">
+    <span>Selecciono: {{checkbox}} </span>
+    <p><a href="#">¿Ya tengo Cuenta?</a></p>
+  </section>
+
+</template>
+
+<script>
+import infoCake from "@/components/infoCake.vue"
+export default {
+    components:{
+    infoCake
+  },
+  props: ['checkbox']
+    
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.tituInfo {
+  color: #642a73;
+  font-size: 30px;
+  text-align: center;
+  margin-top: 40px;
+  background: #728b8f56;
+}
+
+.form-register {
+  width: 400px;
+  background: #160b5ef8;
+  padding: 30px;
+  margin: auto;
+  margin-top: 20px;
+  border-radius: 4px;
+  font-family: 'calibri';
+  color: white;
+  box-shadow: 7px 13px 37px rgba(184, 33, 214, 0.529);
+}
+
+.form-register h4 {
+  font-size: 22px;
+  margin-bottom: 20px;
+}
+
+.controls {
+  width: 100%;
+  background: #fff;
+  padding: 10px;
+  border-radius: 4px;
+  margin-bottom: 16px;
+  border: 1px solid #fff;
+  font-family: 'calibri';
+  font-size: 18px;
+  color: white;
+}
+
+.controls #placeholder{
+    color: #fff;
+}
+
+
+.form-register p {
+  height: 40px;
+  text-align: center;
+  font-size: 18px;
+  line-height: 40px;
+}
+
+.form-register a {
+  color: white;
+  text-decoration: none;
+}
+
+.form-register a:hover {
+  color: white;
+  text-decoration: underline;
+}
+
+.form-register .botons {
+  width: 100%;
+  background: #dc0ca1fd;
+  border: none;
+  padding: 12px;
+  color: white;
+  margin: 16px 0;
+  cursor: pointer;
+  font-size: 16px;
+}
+</style>
---
+++ src/components/tasteCake.vue
@@ -0,0 +1,135 @@
+<template>
+  <section class="portafolio" id="menu">
+    <div class="contenedor">
+      <h2 class="titulo">Menú</h2>
+      <div class="galeria-port">
+        <div class="imagen-port">
+          <img src="../assets/chocolate.jpg" alt="" />
+          <h2>chocolate</h2>
+          <div class="hover-galeria">
+            <a href="pedido.html"
+              ><img src="../assets/bolsa-de-la-compra.png" alt=""
+            /></a>
+            <p>Chocolate</p>
+            <p>$300</p>
+          </div>
+        </div>
+        <div class="imagen-port">
+          <img src="../assets/fresa.jpg" alt="" />
+          <div class="hover-galeria">
+            <a href="pedido.html"
+              ><img src="../assets/bolsa-de-la-compra.png" alt=""
+            /></a>
+            <p>Fresa</p>
+            <p>$400</p>
+          </div>
+        </div>
+        <div class="imagen-port">
+          <img src="../assets/cajeta.jpg" alt="" />
+          <div class="hover-galeria">
+            <a href="pedido.html"
+              ><img src="../assets/bolsa-de-la-compra.png" alt=""
+            /></a>
+            <p>Cajeta</p>
+            <p>$300</p>
+          </div>
+        </div>
+        <div class="imagen-port">
+          <img src="../assets/moras.jpg" alt="" />
+          <div class="hover-galeria">
+            <a href="pedido.html"
+              ><img src="../assets/bolsa-de-la-compra.png" alt=""
+            /></a>
+            <p>Moras</p>
+            <p>$350</p>
+          </div>
+        </div>
+        <div class="imagen-port">
+          <img src="../assets/oreo.jpg" alt="" />
+          <div class="hover-galeria">
+            <a href="pedido.html"
+              ><img src="../assets/bolsa-de-la-compra.png" alt=""
+            /></a>
+            <p>Oreo</p>
+            <p>$400</p>
+          </div>
+        </div>
+        <div class="imagen-port">
+          <img src="../assets/red-velvet.jpg" alt="" />
+          <div class="hover-galeria">
+            <a href="pedido.html"
+              ><img src="../assets/bolsa-de-la-compra.png" alt=""
+            /></a>
+            <p>Red velvet</p>
+            <p>$450</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.titulo {
+  color: #642a73;
+  font-size: 30px;
+  text-align: center;
+  margin-bottom: 30px;
+}
+.portafolio {
+  background: #f2f2f2;
+  margin-top: 80px;
+}
+.galeria-port {
+  display: flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
+
+.imagen-port {
+  width: 15%;
+  height: auto;
+  margin-bottom: 10px;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+}
+
+.imagen-port > img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  display: block;
+}
+
+.hover-galeria {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  transform: scale(0);
+  background: hsla(273, 91%, 27%, 0.7);
+  transition: transform 0.5s;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.hover-galeria img {
+  width: 50px;
+}
+
+.hover-galeria p {
+  color: #fff;
+}
+
+.imagen-port:hover .hover-galeria {
+  transform: scale(1);
+}
+</style>
